16e2d917dc916e092cde2aa2b856ef8eac45ec79
kechankrisna <ke.chankrisna168@gmail.com>
2022-04-17 20:35:01 +0700

    imp api v1

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
+const router = express.Router();
+const apiV1 = require('./api_v1')
 
 app.get('/', (req, res) => {
     res.json({ message: 'Docker is easy' })
 });
+
+app.use('/api/v1/', apiV1)
 
 const port = process.env.port || 8080;
 
